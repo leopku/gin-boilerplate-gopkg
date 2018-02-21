c1243bf9b11df2026c42ed2c858123a3febc857b
leopku <leopku@qq.com>
2018-02-21 22:59:55 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+/**
+* File: /main.go
+* File Created: Wednesday, 21st February 2018 9:59:13 pm
+* Author: liusong (liusong@daojia.com)
+* -----
+* Last Modified: Wednesday, 21st February 2018 9:59:13 pm
+* Modified By: liusong (liusong@daojia.com>)
+* -----
+* Copyright 2018 - 2018 daojia.com, daojia
+*/
+package main
+
+import (
+  _ "gopkg.in/gin-gonic/gin.v1"
+  log "gopkg.in/sirupsen/logrus.v1"
+)
+
+func main() {
+  log.Info("asdfasdf")
+}
