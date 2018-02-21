46a9f23cd5620fe7140a09b7c15890b4bad38421
leopku <leopku@qq.com>
2018-02-21 23:32:28 +0800

    load env

--- main.go
+++ main.go
@@ -11,10 +11,14 @@
 package main
 
 import (
+	"os"
   _ "gopkg.in/gin-gonic/gin.v1"
   log "gopkg.in/sirupsen/logrus.v1"
+  "gopkg.in/joho/godotenv.v1"
 )
 
 func main() {
-  log.Info("asdfasdf")
+  // err := godotenv.Load()
+  godotenv.Load()
+  log.Info("[asdf]" + os.Getenv("DEMO"))
 }
